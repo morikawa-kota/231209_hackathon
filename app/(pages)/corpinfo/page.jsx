af1a68b78e76e8a308fa83d805ab7e0c38c6eff4
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2023-12-10 15:09:18 +0900

    fixed: 企業側の入力ページの外見の整形

--- app/(pages)/corpinfo/page.jsx
+++ app/(pages)/corpinfo/page.jsx
@@ -7,25 +7,122 @@ import style from '../../corpinfo.module.css';
 export default function CorporateInfo() {
     const [optionOpen, setOptionOpen] = useState(false);
     const [isDeleteState, setIsDeleteState] = useState(false);
+    const [campanyName, setCampanyName] = useState('');
+    const [campanyAddress, setCampanyAddress] = useState('');
+    const [campanyDraftName, setCampanyDraftName] = useState('');
+    const [campanyDraftAddress, setCampanyDraftAddress] = useState('');
+    const [jobName, setJobName] = useState('');
+    const [employeeStyle, setEmployeeStyle] = useState('');
+    const [moneyStyle, setMoneyStyle] = useState('');
+    const [jobTask, setJobTask] = useState('');
+    const [tags, setTags] = useState('');
+    const [jobDraftName, setJobDraftName] = useState('');
+    const [employeeDraftStyle, setEmployeeDraftStyle] = useState('');
+    const [moneyDraftStyle, setMoneyDraftStyle] = useState('');
+    const [jobDraftTask, setJobDraftTask] = useState('');
+    const [draftTags, setDraftTags] = useState('');
+
     const toggleOption = async () => {
         setOptionOpen(!optionOpen);
     };
 
+    const saveCampanyInfo = () => {
+        console.log(campanyName);
+        console.log(campanyAddress);
+
+        setCampanyName(campanyDraftName);
+        setCampanyAddress(campanyDraftAddress);
+
+        const res = fetch("/api/campany/", {
+            method: "PUT",
+            body: JSON.stringify(
+              {
+                name: campanyDraftName,
+                adress: campanyDraftAddress,
+              }
+            ),
+          }).catch((error) => {
+            console.error(error);
+        });
+
+        console.log(res);
+    };
+
+    const cancelCampanyInfo = () => {
+        setCampanyDraftName(campanyName);
+        setCampanyDraftAddress(campanyAddress);
+    }
+
+    const saveWorkInfo = () => {
+        setJobName(jobDraftName);
+        setEmployeeStyle(employeeDraftStyle);
+        setMoneyStyle(moneyDraftStyle);
+        setJobTask(jobDraftTask);
+        setTags(draftTags);
+
+        fetch("/api/campany/", {
+            method: "PUT",
+            body: JSON.stringify(
+              {
+                jobName: jobDraftName,
+                employeeStyle: employeeDraftStyle,
+                moneyStyle: moneyDraftStyle,
+                jobTask: jobDraftTask,
+                tags: draftTags
+              }
+            )
+          }).catch((error) => {
+            console.error(error);
+          });
+    }
+
+    const cancelWorkInfo = () => {
+        setJobDraftName(jobName);
+        setEmployeeDraftStyle(employeeStyle);
+        setMoneyDraftStyle(moneyStyle);
+        setJobDraftTask(jobTask);
+        setDraftTags(tags);
+    }
+
     return (
         <div className={style["main-content"]}>
-            <form className={style["input-form"]}>
+            <div className={style["input-form"]}>
                 <div className={`${style["standard-info"]}`}>
                 <h1>基本情報入力</h1>
                     <div className={style.group}>
                         <div className={style["info-text"]}>会社名</div>
-                        <input type="text" className={style["campany-name"]} placeholder="会社名"></input>
+                        <input 
+                            type="text" 
+                            className={style["campany-name"]} 
+                            placeholder="会社名"
+                            value={campanyDraftName}
+                            onChange={(event) => {
+                                setCampanyDraftName(event.target.value)
+                            }}
+                        ></input>
                     </div>
                     <div className={style.group}>
                         <div className={style["info-text"]}>住所</div>
-                        <input type="text" className={style["campany-address"]} placeholder="住所"></input>
+                        <input 
+                            type="text" 
+                            className={style["campany-address"]} 
+                            placeholder="住所"
+                            value={campanyDraftAddress}
+                            onChange={(event) => {
+                                setCampanyDraftAddress(event.target.value)
+                            }}
+                        ></input>
                     </div>
-                    <button type="submit" className={`${style["button-module"]} ${style["func-submit"]}`}>保存する</button>
-                    <button type="cancel" className={style["button-module"]}>キャンセル</button>
+                    <button 
+                        type="" 
+                        className={`${style["button-module"]} ${style["func-submit"]}`}
+                        onClick={() => saveCampanyInfo()}
+                    >保存する</button>
+                    <button 
+                        type="cancel" 
+                        className={style["button-module"]}
+                        onClick={() => cancelCampanyInfo()}
+                    >キャンセル</button>
                 </div>
 
                 <div className={`${style["job-info"]}`}>
@@ -34,38 +131,94 @@ export default function CorporateInfo() {
                         <div></div>
                     ) : (
                         <>
-                        <h1>仕事情報入力</h1><div className={style.group}>
-                        <div className={style["info-text"]}>仕事名</div>
-                            <input type="text" className={style["job-name"]} placeholder="仕事名"></input>
-                            <div className={`${style["option-icon"]} ${optionOpen ? style.active : ""}`} onClick={toggleOption}>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                            <div className={`${style["option"]} ${optionOpen ? style.active : ""}`} id="option">
-                                <button type='submit' className={style["option-item"]}>編集する</button>
-                                {!isDeleteState && (<button type='submit' className={style["option-item"]} onClick={() => { setIsDeleteState(true); } }>削除する</button>)}
+                        <div className={style.jobInfoHeader}>
+                            <h1>仕事情報入力</h1>
+                            <div className={style.menu}>
+                                <div className={`${style["option-icon"]} ${optionOpen ? style.active : ""}`} onClick={toggleOption}>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>                    
+                                <div className={`${style["option"]} ${optionOpen ? style.active : ""}`} id="option">
+                                    <button className={style["option-item"]}>編集する</button>
+                                    {/* {!isDeleteState && (<button type='submit' className={style["option-item"]} onClick={() => { setIsDeleteState(true); } }>削除する</button>)} */}
+                                    <button className={style["option-item"]}>削除する</button>
+                                </div>
                             </div>
                         </div>
                         <div className={style.group}>
+                        <div className={style["info-text"]}>仕事名</div>
+                            <input 
+                                type="text" 
+                                className={style["job-name"]} 
+                                placeholder="仕事名"
+                                value={jobDraftName}
+                                onChange={(event) => 
+                                    setJobDraftName(event.target.value)
+                                }
+                            ></input>
+                        </div>
+                        <div className={style.group}>
                             <div className={style["info-text"]}>雇用形態</div>
-                                <input type="text" className={`${style["mg-right-5"]} ${style["employee-style"]}`} placeholder="正社員/バイト"></input>
-                                <input type="text" className={`${style["mg-right-5"]} ${style["money-style"]}`} placeholder="月給/時給"></input>
+                                <input 
+                                    type="text" 
+                                    className={`${style["mg-right-5"]} ${style["employee-style"]}`} 
+                                    placeholder="正社員/バイト"
+                                    value={employeeDraftStyle}
+                                    onChange={(event) => {
+                                        setEmployeeDraftStyle(event.target.value)
+                                    }}
+                                ></input>
+                                <input 
+                                    type="text" 
+                                    className={`${style["mg-right-5"]} ${style["money-style"]}`} 
+                                    placeholder="月給/時給"
+                                    value={moneyDraftStyle}
+                                    onChange={(event) => {
+                                        setMoneyDraftStyle(event.target.value)
+                                    }}
+                                ></input>
                             </div>
                             <div className={style.group}>
                                 <div className={style["info-text"]}>仕事内容</div>
-                                <textarea className={style["input-text-area"]} rows={'5'} cols={'33'} placeholder='仕事内容(詳細)'></textarea>
+                                <textarea 
+                                    className={style["input-text-area"]} 
+                                    rows={'5'} 
+                                    cols={'33'} 
+                                    placeholder='仕事内容(詳細)'
+                                    value={jobDraftTask}
+                                    onChange={(event) => {
+                                        setJobDraftTask(event.target.value)
+                                    }}
+                                ></textarea>
                             </div>
                             <div className={style.group}>
                                 <div className={style["info-text"]}>メディア</div>
-                                <input type="file"></input>
+                                <input type="file" accept='image/*'></input>
                             </div>
                             <div className={style.group}>
                                 <div className={style["info-text"]}>タグ</div>
-                                <textarea className={`${style["input-text-area"]} ${style["tag-module"]}`} placeholder="タグを挿入(スペースで区切る)" rows={'1'} cols={'40'}></textarea>
+                                <textarea 
+                                    className={`${style["input-text-area"]} ${style["tag-module"]}`} 
+                                    placeholder="タグを挿入(スペースで区切る)" 
+                                    rows={'1'} 
+                                    cols={'40'}
+                                    value={draftTags}
+                                    onChange={(event) => {
+                                        setDraftTags(event.target.value)
+                                    }}
+                                ></textarea>
                             </div>
-                            <button type="submit" className={`${style["button-module"]} ${style["func-submit"]}`}>保存する</button>
-                            <button type="cancel" className={style["button-module"]}>キャンセル</button>
+                            <button 
+                                type=""
+                                className={`${style["button-module"]} ${style["func-submit"]}`}
+                                onClick={() => saveWorkInfo()}
+                            >保存する</button>
+                            <button 
+                                type=""
+                                className={style["button-module"]}
+                                onClick={() => cancelWorkInfo()}
+                            >キャンセル</button>
                         </>
                     )
                 }
@@ -82,7 +235,7 @@ export default function CorporateInfo() {
                         求人を追加する
                     </button>
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
---
+++ app/api/campany/route.js
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request) {
+  const params = request.nextUrl.searchParams.get("params");
+  const user = {
+    name: "山田 太郎",
+    adress: "住所",
+  };
+  return NextResponse.json(
+    { data: user, status: 200 }
+  );
+}
+
+export async function PUT(request) {
+  const body = await request.json();
+  return NextResponse.json(
+    { status: 200 }
+  );
+}
