02673adff4fc9d0c59c71647d4247912a3d7c4ec
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-10 15:47:36 +0900

    userState != null && item.tags =>userState.tags

--- app/(pages)/dashboard/page.jsx
+++ app/(pages)/dashboard/page.jsx
@@ -6,6 +6,7 @@ import style from "../../dashbord.module.css"
 export default function Dashboard() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [userState, setUserState] = useState(null);
   
   const joinWork = () => {
     const fetchtData = async () => {
@@ -34,8 +35,21 @@ export default function Dashboard() {
         });
         const responseJSON = await response.json();
         const workList = responseJSON.data;
+        console.log("-------")
         setData(workList);
         setIsLoading(false);
+      }
+      catch (error) {
+        console.error(error);
+      }
+      try {
+        const userId = 1;
+        const response = await fetch(`/api/user/?params=${userId}`, {
+          method: "GET",
+        });
+        const data = await response.json();
+        const userData = data.data;
+        setUserState(userData);
       }
       catch (error) {
         console.error(error);
@@ -43,17 +57,20 @@ export default function Dashboard() {
     }
     fetchtData();
   }, []);
+  
 
   return <>
     {isLoading ? (<p>Loading...</p>) : <div className={style.wrapper}>
       <h1 className={style.h1}>あなたへのおすすめ</h1>
       <div className={style.cardList}>
         {data.length && (
-          data.map((item, key) => (
-            <div key={key}>
-            <Card data={item} onClick={joinWork} />
-            </div>
-          ))
+          data.map((item, key) => {
+            if (userState != null && item.tags.some((tag) => userState.tags.includes(tag))) {
+              return (<div key={key}>
+                <Card data={item} onClick={joinWork} />
+              </div>)
+            }
+          })
         )}
       </div>
       <h1 className={style.h1}>募集中の求人</h1>
