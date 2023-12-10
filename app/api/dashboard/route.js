0b6d04f2553ee74f8451681c1f39201da02f993e
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-10 15:47:47 +0900

    demo data

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -1,28 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request) {
+  // const fetchtData = async () => {
+  //   try {
+  //     await fetch(`https://lolup.lolbox.jp/get_recruits/`, {
+  //       method: "GET",
+  //     }).then((response) => {
+  //       const workList = response;
+  //       console.log(workList)
+  //       return NextResponse.json(
+  //         { data: workList, status: 200 }
+  //       );
+  //     })
+  //   }
+  //   catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  // fetchtData();
+
   const workList = [{
     place: '沖縄市',
     hourlyPay: 1000,
-    time: '10時~22時'
+    time: '10時~22時',
+    tags: ["語学","料理"]
   },
   {
     place: '沖縄市',
     hourlyPay: 1000,
-    time: '10時~22時'
+    time: '10時~22時',
+    tags: ["ああ", "料理"]
     },
     {
       place: '沖縄市',
       hourlyPay: 1000,
-      time: '10時~22時'
+      time: '10時~22時',
+      tags: ["いい", "料理"]
     },
     {
       place: '沖縄市',
       hourlyPay: 1000,
-      time: '10時~22時'
+      time: '10時~22時',
+      tags: ["うう", "料理"]
     }];
   return NextResponse.json(
-    { data: workList, status: 200 }
+    { data: workList, status: 500 }
   );
 }
 
