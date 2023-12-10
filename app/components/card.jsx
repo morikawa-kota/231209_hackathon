8216ed0da6f907156a7281dc183f88191fb3cc5a
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-10 12:32:02 +0900

    [fix] snakcase

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 import style from "./card.module.css"
 
-export default function Card({ data }) {
+export default function Card({ data, onClick }) {
   return (
     <div className={style.card}>
       <img src="" className={style.img} />
-      <div className={style.card-data}>
+      <div className={style.cardData}>
         <p>場所:　{data.place}</p>
         <p>時給:　{data.hourlyPay}円</p>
         <p>時間:　{data.time}</p>
         <p>距離:　（ユーザー情報から概算）</p>
       </div>
-      <button className={style.joinButton} type="button">応募する</button>
+      <button className={style.joinButton} onClick={onClick} type="button">応募する</button>
     </div>
   )
 }
