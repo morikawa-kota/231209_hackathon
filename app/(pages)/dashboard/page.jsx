2c87c6117901636ad03ea3d33dd02c21d566383d
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-10 12:32:46 +0900

    [add] data fetch

--- app/(pages)/dashboard/page.jsx
+++ app/(pages)/dashboard/page.jsx
@@ -5,25 +5,59 @@ import style from "../../dashbord.module.css"
 
 export default function Dashboard() {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  
+  const joinWork = () => {
+    const fetchtData = async () => {
+      try {
+        await fetch(`/api/dashboard/`, {
+          method: "POST",
+          body: JSON.stringify("userdata"),
+        }).then((response) => {
+          const data = response.json()
+          console.log(data)
+        })
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+    fetchtData();
+  }
 
   useEffect(() => {
     setIsLoading(true);
-    const JSON = {
-      place: '沖縄市',
-      hourlyPay: 1000,
-      time: '10時~22時'
+    const fetchtData = async () => {
+      try {
+        const response = await fetch(`/api/dashboard/`, {
+          method: "GET",
+        });
+        const responseJSON = await response.json();
+        const workList = responseJSON.data;
+        setData(workList);
+        setIsLoading(false);
+      }
+      catch (error) {
+        console.error(error);
+      }
     }
-    setData(JSON);
-    setIsLoading(false);
+    fetchtData();
   }, []);
 
   return <>
-    {isLoading ? (<p>Loading...</p>) : <div className={style.warrapper}>
-      <h1 className={style.h1}>あなたへのおすすめ</h1 >
-      <Card data={data} /> 
+    {isLoading ? (<p>Loading...</p>) : <div className={style.wrapper}>
+      <h1 className={style.h1}>あなたへのおすすめ</h1>
+      <div className={style.cardList}>
+        {data.length && (
+          data.map((item) => (<Card data={item} onClick={joinWork} />))
+        )}
+      </div>
       <h1 className={style.h1}>募集中の求人</h1>
-      <Card data={data} />
+      <div className={style.cardList}>
+        {data.length && (
+          data.map((item) => (<Card data={item} onClick={joinWork} />))
+        )}
+      </div>
     </div >}
   </> 
 }
